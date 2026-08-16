ff2bc9eb076c171d257f7c34920162eabf00110d
mdhsaikats <saikatsikder2911@gmail.com>
2026-08-17 04:16:36 +0600

    changes

--- src/database/db.js
+++ src/database/db.js
@@ -6,13 +6,16 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = process.env.DATABASE_URL
-  ? new Pool({ connectionString: process.env.DATABASE_URL })
+  ? new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.DB_SSL === "false" ? false : { rejectUnauthorized: false },
+  })
   : new Pool({
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    });
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
 
 export default pool;
